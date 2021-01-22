f08e682abacfc7b8b4f24b77e0f0e6025fbecbc7
Vox Humana <jonprine@gmail.com>
2021-01-22 16:48:10 -0600

    added funtion of ending song to next

--- script.js
+++ script.js
@@ -99,29 +99,35 @@ function updateProgressBar(e) {
     progress.style.width = `${progressPercent}%`;
     // calculate display for duration
     const durationMinutes = Math.floor(duration / 60);
-    console.log("minutes", durationMinutes);
     let durationSeconds = Math.floor(duration % 60);
     if (durationSeconds < 10) {
       durationSeconds = `0${durationSeconds}`;
     }
-    console.log("seconds", durationSeconds);
     // delay switching duration element to avoid nan
     if (durationSeconds) {
       durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
     }
     // calculate display for current
     const currentMinutes = Math.floor(currentTime / 60);
-    console.log("minutes", currentMinutes);
     let currentSeconds = Math.floor(currentTime % 60);
     if (currentSeconds < 10) {
       currentSeconds = `0${currentSeconds}`;
     }
-    console.log("seconds", currentSeconds);
     currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
   }
+}
+
+// set progress bar
+function setProgressBar(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const { duration } = music;
+    music.currentTime = (clickX / width) * duration;
 }
 
 // event listeners
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar)
